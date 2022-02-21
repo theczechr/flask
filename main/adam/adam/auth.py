466e293eed5c4fc603a00ae96f4b710ad6a36249
theczechr <robinjarusek@gmail.com>
2022-02-21 11:18:38 +0100

    a lotta things

---
+++ main/adam/adam/auth.py
@@ -0,0 +1,29 @@
+from flask import Blueprint, render_template, redirect, url_for, request, flash
+from .models import User
+from flask_login import login_user
+from . import db
+
+
+auth = Blueprint('auth', __name__)
+@auth.route('/login') 
+def login():
+    return render_template('login.html') # samotna login stranka
+
+@auth.route('/login', methods=['POST'])
+def login_post(): # kontrola loginu
+    name = request.form.get('username')
+    password = request.form.get('password')
+    remember = True if request.form.get('remember') else False
+
+    user = User.query.filter_by(name=name).first()
+
+    # check if the user actually exists
+    # take the user-supplied password, hash it, and compare it to the hashed password in the database
+    if not user or user.password != password:
+        flash('Please check your login details and try again.')
+        return redirect(url_for('auth.login')) # if the user doesn't exist or password is wrong, reload the page
+
+    # if the above check passes, then we know the user has the right credentials
+    login_user(user, remember=remember)
+    return redirect(url_for('main.rozcestnik'))
+
